606abb71929bbbe7432382c71d1143219cd58d5a
Brunhilde <brandontu35@gmail.com>
2025-03-26 17:22:21 -0400

    feat added basic styling for the extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [spinner, setSpinner] = useState(false);
+  const [sumDisplay, setSumDisplay] = useState(false);
+  const exOutput = ["test1", "test2", "test3"]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>T.L.D.R.T.</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => {setSumDisplay(!sumDisplay); setSpinner(!spinner);}}>
+          Summarize This Page Teto!
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {
+          sumDisplay && (<ul>
+            {exOutput.map(output => (
+              <li>{output}</li>
+            ))}
+          </ul>)
+        }
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
