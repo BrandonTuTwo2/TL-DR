7b99044a8c52fecf62a87adbb74464219bbbb64b
Brunhilde <brandontu35@gmail.com>
2025-04-17 18:31:27 -0400

    feat ported over some of the stuff from summarize to server/api since we'll have to run it locally b/c netlify will time gate me cause the AI takes too long

---
+++ server/api.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import OpenAI from 'openai';
+import { config } from "dotenv";
+
+const api = express();
+
+config();
+const openai = new OpenAI({
+  baseURL: 'https://openrouter.ai/api/v1',
+  apiKey: process.env.ROUTER_API,
+  timeout: 120000 // 120 seconds
+});
+
+const port = process.env.PORT || 3000;
+const jsonParser = bodyParser.json();
+
+const corsOptions = {
+    origin: '*',
+    credential: true,
+    optionSuccessStatus: 200,
+}
+
+api.use(cors(corsOptions));
+
+api.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+
+console.log("HUH??");
+
+api.post("/summarize", jsonParser, async(req,res) => {
+    console.log("Here is req")
+    console.log(req.body.url);
+
+    const completion = await openai.chat.completions.create({
+        model: 'google/gemini-2.0-flash-exp:free',
+        messages: [
+          {
+            role: 'user',
+            content: [
+              {
+                type: "text",
+                text: `summarize this article into bullet points for me using only information from this link: ${req.body.url}`
+              }
+            ],
+          },
+        ],
+      });
+
+      console.log("DONE!")
+      console.log(completion)
+      console.log(completion.choices[0].message.content);
+    res.send({
+        body: completion.choices[0].message.content
+    })
+})
+
--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,21 @@ function App() {
 
   const fetchSummary = async () => {
     console.log("Hi ME");
+    const header = new Headers(); // I love cors
+    header.append('access-control-allow-credentials','true');
+    header.append('Content-Type','application/json; charset=utf-8')
+
     //grab url of page
     setSpinner(!spinner);
     //const tab = await chrome.tabs.query({active: true, lastFocusedWindow: true})
     const tab = "https://gamerant.com/omori-best-ending-guide/"
     //we might need a dotenv here
-    const fetchURL = 'https://tl-drt.netlify.app/.netlify/functions/summarize'
+    //const fetchURL = 'https://tl-drt.netlify.app/.netlify/functions/summarize'
     //const fetchURL = 'http://localhost:8888/.netlify/functions/summarize'
+    const fetchURL = 'http://localhost:3000/summarize' //Sadly we'll have to run this locally cause im poor
     const res =  await fetch(fetchURL,{
       method: 'POST',
+      headers: header,
       body: JSON.stringify({
         url:tab
       })
@@ -25,6 +31,7 @@ function App() {
     const filtered = await res.json();
     setSpinner(!spinner);
     setSumDisplay(!sumDisplay);
+    console.log("HI ME");
     console.log(filtered);
   }
 
